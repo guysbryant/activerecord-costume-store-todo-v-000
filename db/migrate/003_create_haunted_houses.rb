7860dccb17b1ff8ca7c58b4036cb59c640831885
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-13 00:13:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
             t.string :family_friendly
             t.datetime :opening_date
             t.datetime :closing_date
-            t.string :description
+            t.string :long_description
         end
     end
 end
