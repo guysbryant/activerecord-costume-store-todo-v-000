1a4481a572ac31c82ad0adad48701073e0e52b85
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-13 00:15:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.string "description"
+    t.text "long_description"
   end
 
 end
